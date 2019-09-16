4d6d50d22e618edb08c3c1eea9b01456fc36198b
Aaron Lander <36257200+aaronlander93@users.noreply.github.com>
2019-09-15 22:25:28 -0700

    Checks implemented for both sides.

--- ChessGame/CheckMate.cpp
+++ ChessGame/CheckMate.cpp
@@ -61,11 +61,11 @@ bool CheckMate::isCheckWhite(map<int, int> squareToPiece, int square) {
 		return true;
 	}
 	//Rook attack
-	for (int i = square + 8; i <= 64; i += 8) {
+	for (int i = square + 8; i < 64; i += 8) {
 		if (squareToPiece[i] == -2 || squareToPiece[i] == -5) {
 			return true;
 		}
-		else if (squareToPiece[i] > 0) {
+		else if (squareToPiece[i] != 0) {
 			break;
 		}
 	}
@@ -74,19 +74,19 @@ bool CheckMate::isCheckWhite(map<int, int> squareToPiece, int square) {
 		if (squareToPiece[i] == -2 || squareToPiece[i] == -5) {
 			return true;
 		}
-		else if (squareToPiece[i] > 0) {
+		else if (squareToPiece[i] != 0) {
 			break;
 		}
 	}
 
-	for (int i = square + 1; i <= 64; i++) {
+	for (int i = square + 1; i < 64; i++) {
 		if (i % 8 == 0) {
 			break;
 		}
 		else if (squareToPiece[i] == -2 || squareToPiece[i] == -5) {
 			return true;
 		}
-		else if (squareToPiece[i] > 0) {
+		else if (squareToPiece[i] != 0) {
 			break;
 		}
 	}
@@ -95,7 +95,7 @@ bool CheckMate::isCheckWhite(map<int, int> squareToPiece, int square) {
 		if (squareToPiece[i] == -2 || squareToPiece[i] == -5) {
 			return true;
 		}
-		else if (squareToPiece[i] > 0) {
+		else if (squareToPiece[i] != 0) {
 			break;
 		}
 		else if (i % 8 == 0) {
@@ -170,5 +170,116 @@ bool CheckMate::isCheckWhite(map<int, int> squareToPiece, int square) {
 }
 
 bool CheckMate::isCheckBlack(map<int, int> squareToPiece, int square) {
+	//Pawn attack
+	if (squareToPiece[square + 7] == 1 || squareToPiece[square + 9] == 1) {
+		return true;
+	}
+	//Rook attack
+	for (int i = square + 8; i < 64; i += 8) {
+		if (squareToPiece[i] == 2 || squareToPiece[i] == 5) {
+			return true;
+		}
+		else if (squareToPiece[i] != 0) {
+			break;
+		}
+	}
+
+	for (int i = square - 8; i >= 0; i -= 8) {
+		if (squareToPiece[i] == 2 || squareToPiece[i] == 5) {
+			return true;
+		}
+		else if (squareToPiece[i] != 0) {
+			break;
+		}
+	}
+
+	for (int i = square + 1; i < 64; i++) {
+		if (i % 8 == 0) {
+			break;
+		}
+		else if (squareToPiece[i] == 2 || squareToPiece[i] == 5) {
+			return true;
+		}
+		else if (squareToPiece[i] != 0) {
+			break;
+		}
+	}
+
+	for (int i = square - 1; i >= 0; i--) {
+		if (squareToPiece[i] == 2 || squareToPiece[i] == 5) {
+			return true;
+		}
+		else if (squareToPiece[i] != 0) {
+			break;
+		}
+		else if (i % 8 == 0) {
+			break;
+		}
+	}
+	//Knight attack
+	if (squareToPiece[square + 6] == 3 || squareToPiece[square - 6] == 3) return true;
+	if (squareToPiece[square + 10] == 3 || squareToPiece[square - 10] == 3) return true;
+	if (squareToPiece[square + 15] == 3 || squareToPiece[square - 15] == 3) return true;
+	if (squareToPiece[square + 17] == 3 || squareToPiece[square - 17] == 3) return true;
+
+	//Bishop attack
+	for (int i = square + 9; i <= 64; i += 9) {
+		if (squareToPiece[i] == 4 || squareToPiece[i] == 5) {
+			return true;
+		}
+		else if (squareToPiece[i] != 0) {
+			break;
+		}
+		if ((i + 1) % 8 == 0) {
+			break;
+		}
+	}
+
+	for (int i = square - 9; i >= 0; i -= 9) {
+		if (squareToPiece[i] == 4 || squareToPiece[i] == 5) {
+			return true;
+		}
+		else if (squareToPiece[i] != 0) {
+			break;
+		}
+		if (i % 8 == 0) {
+			break;
+		}
+	}
+
+	for (int i = square + 7; i <= 64; i += 7) {
+		if (squareToPiece[i] == 4 || squareToPiece[i] == 5) {
+			return true;
+		}
+		else if (squareToPiece[i] != 0) {
+			break;
+		}
+		if (i % 8 == 0) {
+			break;
+		}
+	}
+
+	for (int i = square - 7; i >= 0; i -= 7) {
+		if (squareToPiece[i] == 4 || squareToPiece[i] == 5) {
+			return true;
+		}
+		else if (squareToPiece[i] != 0) {
+			break;
+		}
+		if ((i + 1) % 8 == 0) {
+			break;
+		}
+	}
+
+	//King attack
+	if (squareToPiece[square - 1] == 6) return true;
+	if (squareToPiece[square + 1] == 6) return true;
+	if (squareToPiece[square - 8] == 6) return true;
+	if (squareToPiece[square + 8] == 6) return true;
+	if (squareToPiece[square - 7] == 6) return true;
+	if (squareToPiece[square + 7] == 6) return true;
+	if (squareToPiece[square - 9] == 6) return true;
+	if (squareToPiece[square + 9] == 6) return true;
+
 	return false;
 }
